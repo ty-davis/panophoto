eb89a5e250ef4d179bfe3b627136e66c1ae18427
Ty Davis <tydavisxc2019@gmail.com>
2026-02-26 18:03:42 -0700

    Added hosting from npm run dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
